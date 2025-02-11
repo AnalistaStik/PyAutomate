8a686e61f956e2df1a5e0960386fb7a99dee8a6f
Anderson-Reis510 <andersonnojosa.dev@gmail.com>
2025-02-11 14:34:28 -0300

    Atualização

--- main.py
+++ main.py
@@ -3,10 +3,74 @@ import subprocess
 import time
 import pyperclip 
 import logging
+import os
+import requests
 import pygetwindow as gw
 from datetime import datetime
-import tkinter as tk
-from tkinter import messagebox
+
+# Configurações do logging 
+logging.basicConfig(
+    filename="log_pedidos.txt",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+# Função para verificar a versão mais recente no GitHub
+def verificar_versao():
+    try:
+        url_releases = "https://api.github.com/repos/AnalistaStik/PyAutomate/releases/latest"
+        
+        response = requests.get(url_releases)
+        
+        release_data = response.json()
+        print("Resposta da API do GitHub:", release_data)
+        
+        if "tag_name" not in release_data:
+            print("Chave 'tag_name' não encontrada na resposta.")
+            return
+        
+        versao_mais_recente = release_data["tag_name"]
+        download_url = release_data["assets"][0]["browser_download_url"]
+        
+        versao_atual = "1.0.0"  
+
+        if versao_atual != versao_mais_recente:
+            print(f"Nova versão disponível: {versao_mais_recente}. Atualizando...")
+            atualizar_programa(download_url)
+        else:
+            print("Você já está utilizando a versão mais recente.")
+    
+    except Exception as e:
+        print(f"Erro ao verificar versão no GitHub: {e}")
+
+# Função para fazer o download e substituir o executável
+def atualizar_programa(download_url):
+    try:
+        resposta = requests.get(download_url)
+        with open("novo_programa.exe", "wb") as file:
+            file.write(resposta.content)
+        
+        print("Novo executável baixado com sucesso. Instalando...")
+
+        if os.path.exists("Rb_Lupo.exe"):
+            os.remove("Rb_Lupo.exe")
+        
+        os.rename("novo_programa.exe", "Rb_Lupo.exe")
+        
+        subprocess.run("Rb_Lupo.exe")
+        
+        print("Programa atualizado e reiniciado com sucesso.")
+    
+    except Exception as e:
+        print(f"Erro ao atualizar o programa: {e}")
+
+# Verificar se há uma nova versão
+verificar_versao()
+
+# Tempos de espera
+time_short = 1
+time_medium = 3
+time_long = 5
 
 # Posições dos elementos na tela       
 coordenadas = {
@@ -23,197 +87,168 @@ coordenadas = {
     "Fechar_janela_navegador": (1893, 19)
 }
 
-# Tempos de espera
-time_short = 1
-time_medium = 3
-time_long = 5
+logging.info("Programa Iniciado")
 
-def start_countdown(seconds):
-    root = tk.Tk()
-    root.title("Contagem Regressiva")
-    root.attributes("-topmost", True)
-    label = tk.Label(root, font=("Helvetica", 20), padx=20, pady=20)
-    label.pack()
+pyautogui.confirm(
+    text='O programa será iniciado. Clique em OK para continuar.', 
+    title='Programa Inicializado', 
+    buttons=['OK']
+)
 
-    def update():
-        nonlocal seconds
-        if seconds >= 0:
-            hours, remainder = divmod(seconds, 3600)
-            mins, secs = divmod(remainder, 60)
-            timer = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
-            label.config(text=f"Tempo restante: {timer}")
-            seconds -= 1
-            # Atualiza a cada 1 segundo
-            root.after(1000, update)
-        else:
-            root.destroy()
-    update()
-    root.mainloop()
-
-def programar_automatizacao():
-    agora = datetime.now()
-    hora_hoje = datetime(agora.year, agora.month, agora.day, 23, 50)
-    
-    # Calcular o tempo restante até as 23:50
-    tempo_restante = (hora_hoje - agora).total_seconds()
-    
-    if tempo_restante > 0:
-        print(f"Aguardando {int(tempo_restante)} segundos até as 23:50...")
-        start_countdown(int(tempo_restante))
-
-programar_automatizacao()
-
-# --- Função que preenche um campo de texto ---
+#Função que preenche um campo de texto
 def preencher_campo(texto):
     pyautogui.press("backspace")
     pyautogui.write(texto)
     
-# --- Função para maximizar uma janela específica ou clicar no botão de maximizar ---
+#Função para maximizar uma janela específica ou clicar no botão de maximizar
 def maximizar_janela(titulo_parcial, coordenada_maximizar=None):
-    time.sleep(time_short)
-    janelas = gw.getWindowsWithTitle(titulo_parcial)
-    if janelas:
-        janela = janelas[0]
-        if not janela.isMaximized:
-            janela.maximize()
+    try:
         time.sleep(time_short)
-    elif coordenada_maximizar:
-        pyautogui.moveTo(coordenada_maximizar)
+        janelas = gw.getWindowsWithTitle(titulo_parcial)
+        if janelas:
+            janela = janelas[0]
+            if not janela.isMaximized:
+                janela.maximize()
+            logging.info(f"Janela '{titulo_parcial}' maximizada.")
+        elif coordenada_maximizar:
+            pyautogui.moveTo(coordenada_maximizar)
+            pyautogui.click()
+            logging.info(f"Maximização via coordenada: {coordenada_maximizar}")
+        time.sleep(time_short)
+    except Exception as e:
+        logging.error(f"Erro ao maximizar a janela {titulo_parcial}: {e}")
+        
+# --- Função que baixa o XML ---
+def baixar_XML():
+    try:
+        time.sleep(time_short)
+        pyautogui.click(button="right")
+        time.sleep(time_short)
+        
+        for _ in range(6):  
+            pyautogui.press("down")
+            time.sleep(0.2)
+
+        pyautogui.press("enter")  
+        time.sleep(time_short)
+
+        pyautogui.moveTo(845, 585)
+        pyautogui.click()
+        time.sleep(time_short)
+        
+        pyautogui.moveTo(903, 460)
         pyautogui.click()
         time.sleep(time_short)
 
-# --- Função que baixa o XML ---
-def baixar_XML():
-    time.sleep(time_short)
-    pyautogui.click(button="right")
-    time.sleep(time_short)
-    
-    for _ in range(6):  
-        pyautogui.press("down")
-        time.sleep(0.2)
+        for _ in range(29):
+            pyautogui.press("down")
+            time.sleep(0.2)
 
-    pyautogui.press("enter")  
-    time.sleep(time_short)
+        pyautogui.press("enter")
+        time.sleep(time_short)
+        logging.info("XML baixado com sucesso.")
 
-    pyautogui.moveTo(845, 585)
-    pyautogui.click()
-    time.sleep(time_short)
-    
-    pyautogui.moveTo(903, 460)
-    pyautogui.click()
-    time.sleep(time_short)
-
-    for _ in range(29):
-        pyautogui.press("down")
-        time.sleep(0.2)
-
-    pyautogui.press("enter")
-    time.sleep(time_short) 
+    except Exception as e:
+        logging.error(f"Erro ao baixar XML: {e}")
         
-# --- Função que baixa o PDF ---
+#Função que baixa o PDF
 def baixar_pedido(pedido_x, pedido_y):
-    time.sleep(time_short)
-    pyautogui.click(button="right")  # Clicar com botão direito no pedido atual
-    time.sleep(time_short)
+    try:
+        time.sleep(time_short)
+        pyautogui.click(button="right")  # Clicar com botão direito no pedido atual
+        time.sleep(time_short)
 
-    for _ in range(11):
-        pyautogui.press("down")
-        time.sleep(0.2)  
+        for _ in range(11):
+            pyautogui.press("down")
+            time.sleep(0.2)  
 
-    pyautogui.press("enter")  
-    time.sleep(time_long)
+        pyautogui.press("enter")  
+        time.sleep(time_long)
 
-    pyautogui.moveTo(coordenadas['Pos. Acrobat'])
-    pyautogui.click()
-    time.sleep(time_long)
+        pyautogui.moveTo(coordenadas['Pos. Acrobat'])
+        pyautogui.click()
+        time.sleep(time_long)
 
-    # Salvar PDF
-    pyautogui.moveTo(1797, 126)  
-    pyautogui.click()
-    time.sleep(time_long)
+        # Salvar PDF
+        pyautogui.moveTo(1797, 126)  
+        pyautogui.click()
+        time.sleep(time_long)
 
-    pyautogui.moveTo(99, 240)  
-    pyautogui.click()
-    time.sleep(time_long)
+        pyautogui.moveTo(99, 240)  
+        pyautogui.click()
+        time.sleep(time_long)
 
-    pyautogui.moveTo(791, 507)  
-    pyautogui.click()
-    time.sleep(time_long)
+        pyautogui.moveTo(791, 507)  
+        pyautogui.click()
+        time.sleep(time_long)
 
-    pyautogui.moveTo(coordenadas['Fechar_janela_navegador'])
-    pyautogui.click()
-    time.sleep(time_long)
-    pyautogui.moveTo(coordenadas['Fechar Janela'])
-    pyautogui.click()
-    time.sleep(time_long) 
+        pyautogui.moveTo(coordenadas['Fechar_janela_navegador'])
+        pyautogui.click()
+        time.sleep(time_long)
+        pyautogui.moveTo(coordenadas['Fechar Janela'])
+        pyautogui.click()
+        time.sleep(time_long) 
+        logging.info("PDF baixado com sucesso.")
+
+    except Exception as e:
+        logging.error(f"Erro ao baixar PDF: {e}")
+    
+def programar_automatizacao():
+    while True:
+        agora = datetime.now()
+        hora_hoje = datetime(agora.year, agora.month, agora.day, 23, 50)
+        # Se o horário chegou, sai do loop e continua o processo
+        if agora >= hora_hoje:
+            print("Hora de começar o processo de automação!")
+            break
+        else:
+            # Espera 10 segundos antes de checar novamente
+            time.sleep(10)
+
+programar_automatizacao()
             
-# --- Entrar e logar no TopManager ---
-time.sleep(time_short)
-pyautogui.hotkey("ctrl", "shift", "t")
-time.sleep(time_medium)
+#Entrar e logar no TopManager
+for _ in range(3):
+    try:
+        time.sleep(time_medium)
+        pyautogui.hotkey("ctrl", "shift", "t")
+        time.sleep(time_medium)
 
-pyautogui.moveTo(398, 346)
-time.sleep(time_short)
-pyautogui.click()
-preencher_campo("123456")
-pyautogui.press("enter")
-time.sleep(20)
+        pyautogui.moveTo(398, 346)
+        time.sleep(time_short)
+        pyautogui.click()
+        preencher_campo("123456")
+        pyautogui.press("enter")
+        time.sleep(10)
 
-maximizar_janela("TopManager")
+        if gw.getWindowsWithTitle("TopManager"):
+            logging.info("Login no TopManager realizado com sucesso.")
+            maximizar_janela("TopManager")
+            break
+    except Exception as e:
+        logging.error(f"Erro no login: {e}")
 
-# --- Acessar a aba dos Documentos Eletrônicos ---
-pyautogui.moveTo(coordenadas['Consulta'])
-pyautogui.click()
-time.sleep(time_short)
+#Acessar a aba dos Documentos Eletrônicos
+try:
+    pyautogui.moveTo(coordenadas['Consulta'])
+    pyautogui.click()
+    time.sleep(time_short)
 
-pyautogui.moveTo(coordenadas['Doc. Eletronico'])
-pyautogui.click()
-time.sleep(time_short)
+    pyautogui.moveTo(coordenadas['Doc. Eletronico'])
+    pyautogui.click()
+    time.sleep(time_short)
 
-pyautogui.moveTo(coordenadas['Doc_Eletronico'])
-pyautogui.click()
-time.sleep(time_short)
+    pyautogui.moveTo(coordenadas['Doc_Eletronico'])
+    pyautogui.click()
+    time.sleep(time_short)
 
-maximizar_janela('Doc. Eletronico', coordenadas["Maximizar Doc. Eletronico"])
-time.sleep(time_short)
+    maximizar_janela('Doc. Eletronico', coordenadas["Maximizar Doc. Eletronico"])
+    logging.info("Acesso à aba de documentos eletrônicos.")
 
-pyautogui.moveTo(coordenadas['Filtro'])
-pyautogui.click()
-time.sleep(time_short)
+except Exception as e:
+    logging.error(f"Erro ao acessar aba de documentos eletrônicos: {e}")
 
-pyautogui.moveTo(coordenadas['periodo'])
-pyautogui.click(button='left')
-time.sleep(time_short)
-pyautogui.write('o')
-time.sleep(time_short)
-pyautogui.press("tab")
-time.sleep(time_short)
-pyautogui.press("tab")
-time.sleep(time_short)
-preencher_campo('2')
-time.sleep(time_short)
-pyautogui.press('tab')
-time.sleep(time_short)
-pyautogui.press('tab')
-time.sleep(time_short)
-pyautogui.press('tab')
-time.sleep(time_short)
-
-pyautogui.press('enter')
-time.sleep(time_medium)
-preencher_campo('lupo')
-time.sleep(time_short)
-pyautogui.press('enter')
-time.sleep(time_short)
-pyautogui.press('enter')
-time.sleep(time_short)
-pyautogui.press('enter')
-time.sleep(time_short)
-
-pyautogui.press('F5')
-time.sleep(time_long)
-
-# --- Aba dos pedidos e download dos PDF's e XML ---
+# Aba dos pedidos e download dos PDF's e XML
 
 # Configurações
 pedido_x, pedido_y = coordenadas["N. Pedido Base"]
@@ -222,49 +257,39 @@ numero_pedidos = 0
 pedido_anterior = None  # Variável para armazenar o pedido anterior
 
 while True:
-    # Selecionar a célula do pedido atual
-    pyautogui.moveTo(pedido_x, pedido_y)
-    pyautogui.click()
-    time.sleep(time_short)
+    try:
+        pyautogui.moveTo(pedido_x, pedido_y)
+        pyautogui.click()
+        time.sleep(time_short)
 
-    # Copiar o conteúdo
-    pyautogui.hotkey("ctrl", "c")
-    time.sleep(time_short)
-    pedido_atual = pyperclip.paste().strip()
+        pyautogui.hotkey("ctrl", "c")
+        time.sleep(time_short)
+        pedido_atual = pyperclip.paste().strip()
 
-    # Verificar se a célula está vazia (último pedido atingido)
-    if not pedido_atual:
-        print("Nenhum pedido encontrado ou fim da lista.")
-        break
+        if not pedido_atual:
+            logging.info("Nenhum pedido encontrado ou fim da lista.")
+            break
 
-    # Verificar se o pedido atual é o mesmo que o anterior
-    if pedido_atual == pedido_anterior:
-        print("Pedido repetido, encerrando a busca")
-        pedido_y += incremento_y  # Pular para a próxima linha
-        break
+        if pedido_atual == pedido_anterior:
+            logging.info("Pedido repetido, encerrando busca.")
+            break
 
-    print(f"Pedido encontrado: {pedido_atual}")
-    numero_pedidos += 1
+        logging.info(f"Baixando pedido: {pedido_atual}")
+        numero_pedidos += 1
 
-    # Baixar o XML
-    baixar_XML()
+        baixar_XML()
+        baixar_pedido(pedido_x, pedido_y)
 
-    # Baixar o PDF
-    baixar_pedido(pedido_x, pedido_y)
+        pedido_anterior = pedido_atual
+        pedido_y += incremento_y
 
-    # Atualizar o pedido anterior
-    pedido_anterior = pedido_atual
+    except Exception as e:
+        logging.error(f"Erro no processo de download: {e}")
 
-    # Mover para a próxima linha
-    pedido_y += incremento_y
+logging.info(f"Total de pedidos baixados: {numero_pedidos}")
 
-print(f"Total de pedidos baixados: {numero_pedidos}")
-
-time.sleep(time_medium)  
-pyautogui.moveTo(1895, 10)
-pyautogui.click()
-
-# --- Notifica o usuário com um pop-up ao final do processo ---
-pyautogui.alert(text=f"Total de pedidos baixados: {numero_pedidos}", 
-                title="Processo Concluído", 
-                button="OK")
+pyautogui.alert(
+    text=f"Total de pedidos baixados: {numero_pedidos}", 
+    title="Processo Concluído", 
+    button="OK"
+)
